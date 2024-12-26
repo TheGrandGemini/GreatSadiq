452f05f48a70e5671cbf8f125a657290804cee3d
0xgemini <thegrandgemini@gmail.com>
2024-12-26 09:39:20 -0800

    first commit

---
+++ main.js
@@ -0,0 +1,31 @@
+"use strict";
+
+// function toggleMenu() {
+//   const menu = document.querySelector(".navList");
+//   menu.classList.toggle("active");
+// }
+
+// function toggleMenu() {
+//   const menu = document.querySelector("navList");
+//   console.log("Menu clicked");
+//   menu.classList.toggle(".active");
+// }
+
+function toggleMenu() {
+  const menu = document.querySelector(".navList");
+  console.log(menu); // Check if menu is found
+  if (menu) {
+    menu.classList.toggle("active");
+  } else {
+    console.error("Nav list not found!");
+  }
+}
+
+document.addEventListener("click", function (event) {
+  const menu = document.querySelector(".navList");
+  const sandwich = document.querySelector(".sandwichBar");
+  if (!menu.contains(event.target) && !sandwich.contains(event.target)) {
+    menu.classList.remove("active");
+    sandwich.classList.remove("active");
+  }
+});
